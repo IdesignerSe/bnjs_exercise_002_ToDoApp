88492beec38b241e3af875ccfca8ed68fbfcfc7b
IdesignerSe <hebera75@gmail.com>
2023-10-18 09:59:24 +0200

    Just minors detail fixed

--- index.tsx
+++ index.tsx
@@ -1,11 +1,10 @@
 Bun.serve({
-    hostname: "localhost"
-    port: 6000,
+    hostname: "localhost",
+    port: 4000,
     fetch: fetchHandler
-})
+});
 
 function fetchHandler(request: Request): Response {
 
-    return new Response(Bun.file("index.html"))
-
+    return new Response(Bun.file("index.html"));
 }

1b404f8c440abe39890d70c9be50911207f52b51
IdesignerSe <hebera75@gmail.com>
2023-10-18 11:56:09 +0200

    Resume
    Creating the todo catching

--- index.tsx
+++ index.tsx
@@ -1,10 +1,34 @@
-Bun.serve({
+import { type } from "os";
+import {renderToString} from "react-dom/server";
+
+const server = Bun.serve({
     hostname: "localhost",
     port: 4000,
     fetch: fetchHandler
 });
 
-function fetchHandler(request: Request): Response {
+console.log(`Bun Todo running on ${server.hostname}: ${server.port}`);
 
-    return new Response(Bun.file("index.html"));
+type Todo = { id: number; name: string};
+const todos: Todo[] = [];
+
+function fetchHandler(request: Request): Response {
+    const url = new URL(request.url)
+    if(url.pathname === "" || url.pathname === "/"){
+        return new Response(Bun.file("index.html"));
+    }
+
+    if(url.pathname === "/todos" && request.method === "GET"){
+        return new Response(renderToString(<TodoList todos={todos } />));
+    }
+
+    return new Response ("Not Found", { status: 404});
+}
+
+function TodoList(props: { todos: Todo[] }) {
+    return (
+    <ul>
+        {props.todos.length ? props.todos.map((todo) => <li>{todo.name}</li>) : "No todo found"}
+    </ul>
+    );
 }
